42c0b5b9c9c7033b45ac5ed28c40dc89a911db74
JoelEncinas <joelencinasmartin@gmail.com>
2022-01-21 02:37:57 +0100

    ideas for currency

--- Assets/Scripts/Quest/Battlepass.cs
+++ Assets/Scripts/Quest/Battlepass.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class Battlepass : MonoBehaviour
 {
+
+    /* 
+
+    ideas for currency system
+
+    - create prefab for items to buy
+    - login system (implement later)
+    - load items based on whats available (to player/seasonal etc)
+    - currency trading system
+    
+    code implementation
+    - make currency and user info static/save?
+    - currency manager -- dontDestroyOnLoad object
+
+    */
     // Start is called before the first frame update
     void Start()
     {

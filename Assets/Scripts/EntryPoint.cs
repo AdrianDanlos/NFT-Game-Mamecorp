5ea5fdf01c79496c6d699fa5e5a59906dd56037c
AdrianDanlos <adrianf1team@gmail.com>
2022-01-25 18:23:43 +0700

    code refactoring

--- Assets/Scripts/EntryPoint.cs
+++ Assets/Scripts/EntryPoint.cs
@@ -1,16 +1,19 @@
 using System.IO;
 using UnityEngine;
 using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
 
 
 public class EntryPoint : MonoBehaviour
 {
     private void Awake()
     {
-        ApplicationStart();
+        //FIXME: In the future recheck if its possible to group them under a single function e.g. ReadOrCreateSaveFiles that contains a loop.
+        ReadOrCreateUserFile();
+        ReadOrCreateFighterFile();
     }
 
-    public static void ApplicationStart()
+    public static void ReadOrCreateUserFile()
     {
         if (File.Exists(JsonDataManager.getFilePath("user")))
         {
@@ -23,21 +26,43 @@ public class EntryPoint : MonoBehaviour
             //TODO: Pedir nombre al usuario en escena
             JObject user = JObject.FromObject(CreateUserInstance());
             JsonDataManager.SaveData(user, "user");
-        }
-        
-        //TODO: Do the same for fighter
+        };
     }
 
-    public static User CreateUserInstance(string userName = "Berthold", int wins = 0, int loses = 0, int elo = 0)
+    public static void ReadOrCreateFighterFile()
+    {
+        if (File.Exists(JsonDataManager.getFilePath("fighter")))
+        {
+            var fighterData = JsonDataManager.ReadData("fighter");
+            Fighter fighter = CreateFighterInstance();
+            Debug.Log(fighter.fighterName);
+        }
+        else
+        {
+            JObject fighter = JObject.FromObject(CreateSerializableFighterInstance());
+            JsonDataManager.SaveData(fighter, "fighter");
+        }
+    }
+
+    public static User CreateUserInstance(string userName = "UserNameX", int wins = 0, int loses = 0, int elo = 0)
     {
         User user = User.Instance;
         user.SetUserValues(userName, wins, loses, elo);
         return user;
     }
 
-    public static Fighter CreateFighter()
+    //Explain why we need this
+    public static SerializableFighter CreateSerializableFighterInstance()
     {
-        //TODO: Pedir nombre del fighter al usuario en escena
-        return null;
+        //TODO: Pedir nombre al usuario en escena
+        return new SerializableFighter("FighterNameX", 10, 1, 3, "Fire", 1, 10, new List<Card>());
+    }
+
+    public static Fighter CreateFighterInstance()
+    {
+        //Can also be linked through the inspector instead of doing this
+        Fighter fighter = GameObject.Find("Fighter").AddComponent<Fighter>();
+        fighter.FighterConstructor("FighterNameX", 10, 1, 3, "Fire", 1, 10, new List<Card>());
+        return fighter;
     }
 }
---
+++ Assets/Scripts/SerializableFighter.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using System;
+
+[Serializable()]
+public class SerializableFighter {
+    private string _fighterName;
+    private float _hp;
+    private float _damage;
+    private float _speed;
+    private string _species;
+    private int _level;
+    private int _manaSlots;
+    private List<Card> _cards;
+
+    public string fighterName { get => _fighterName; set => _fighterName = value; }
+    public float hp { get => _hp; set => _hp = value; }
+    public float damage { get => _damage; set => _damage = value; }
+    public float speed { get => _speed; set => _speed = value; }
+    public string species { get => _species; set => _species = value; }
+    public int level { get => _level; set => _level = value; }
+    public int manaSlots { get => _manaSlots; set => _manaSlots = value; }
+    public List<Card> cards { get => _cards; set => _cards = value; }
+
+    public SerializableFighter(string fighterName, float hp, float damage, float speed, string species, int level, int manaSlots, List<Card> cards)
+    {
+        this.fighterName = fighterName;
+        this.hp = hp;
+        this.damage = damage;
+        this.speed = speed;
+        this.species = species;
+        this.level = level;
+        this.manaSlots = manaSlots;
+        this.cards = cards;
+    }
+}

e88f6e6c256213de311ab2b41312800e5f737f93
AdrianDanlos <adrianf1team@gmail.com>
2022-02-07 23:33:24 +0700

    go to main menu script reused

---
+++ Assets/Scripts/Utils/OnClickGoToMainMenu.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnClickGoToMainMenu : MonoBehaviour
+{
+    public void GoToMainMenu()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+    }
+}

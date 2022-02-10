fbfcf562ea9e71bab6d3c324bacf2712e1474432
AdrianDanlos <adrianf1team@gmail.com>
2022-02-11 00:46:58 +0700

    fighter name chosen by user is now correctly applied

--- Assets/Scripts/Menus/ChooseFirstFighter/ChooseFirstFighter.cs
+++ Assets/Scripts/Menus/ChooseFirstFighter/ChooseFirstFighter.cs
@@ -2,21 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Newtonsoft.Json.Linq;
+using TMPro;
 
 public class ChooseFirstFighter : MonoBehaviour
 {
+    public GameObject fighterNameInput;
     public void OnSelectFighter()
     {
-        CreateFighterFile();
-        //FIXME: Redirect to main menu after user has given a name to the fighter
-        //Send selected skin to the fighter constructor
+        //FIXME: Save skin
+        GameObject.FindGameObjectWithTag("FighterNamePopup").GetComponent<Canvas>().enabled = true;
+    }
+
+    public void OnConfirmFighterName()
+    {
+        string fighterName = fighterNameInput.GetComponent<TextMeshProUGUI>().text;
+        //FIXME: Send selected skin to the fighter constructor
+        CreateFighterFile(fighterName);
         UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
     }
 
-    private void CreateFighterFile()
+    private void CreateFighterFile(string fighterName)
     {
-        //TODO: Pedir nombre al usuario en escena
-        string fighterName = "fighterNameTypedByUser";
         JObject serializableFighter = JObject.FromObject(JsonDataManager.CreateSerializableFighterInstance(FighterFactory.CreatePlayerFighterInstance(fighterName)));
         JsonDataManager.SaveData(serializableFighter, JsonDataManager.FighterFileName);
     }

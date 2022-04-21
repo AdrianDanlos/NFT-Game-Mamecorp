dc665209310b550a9d34c03c446dfefa07a2d669
AdrianDanlos <adrianf1team@gmail.com>
2022-04-21 11:01:07 +0700

    user data bar changed and reused

--- Assets/Scripts/Menus/Reusables/UserDataBar.cs
+++ Assets/Scripts/Menus/Reusables/UserDataBar.cs
@@ -1,19 +1,27 @@
 using UnityEngine;
 
-public class CollectiblesBar : MonoBehaviour
+public class UserDataBar : MonoBehaviour
 {
     public GameObject gold;
     public GameObject gems;
     public GameObject energy;
     public GameObject timerContainer;
     public GameObject timer;
+    public GameObject playerNameGO;
+    public GameObject playerLevelGO;
+    public GameObject playerExpGO;
+    public GameObject playerLevelSlider;
 
     void Start()
     {
+        Fighter player = PlayerUtils.FindInactiveFighter();
+
         MenuUtils.SetGold(gold);
         MenuUtils.SetGems(gems);
         EnergyManager.RefreshEnergyBasedOnCountdown();
         MenuUtils.SetEnergy(energy);
         MenuUtils.DisplayEnergyCountdown(timerContainer, timer);
+        MenuUtils.SetName(playerNameGO);
+        MenuUtils.SetLevelSlider(playerLevelGO, playerExpGO, playerLevelSlider, player.level, player.experiencePoints);
     }
 }

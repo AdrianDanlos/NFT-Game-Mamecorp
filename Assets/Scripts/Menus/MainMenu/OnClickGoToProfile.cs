866d1ad2fc07faf2277b0f4587cb86cac376242c
JoelEncinas <joelencinasmartin@gmail.com>
2022-04-28 00:26:15 +0200

    added ranking + profile

--- Assets/Scripts/Menus/MainMenu/OnClickGoToProfile.cs
+++ Assets/Scripts/Menus/MainMenu/OnClickGoToProfile.cs
@@ -1,18 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class OnClickGoToProfile : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject profile;
 
-    // Update is called once per frame
-    void Update()
+    public void ShowProfile()
     {
-        
+        profile.SetActive(true);
     }
 }
--- Assets/Scripts/Menus/MainMenu/OnClickGoToRanking.cs
+++ Assets/Scripts/Menus/MainMenu/OnClickGoToRanking.cs
@@ -1,18 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class OnClickGoToRanking : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject ranking;
 
-    // Update is called once per frame
-    void Update()
+    public void ShowRanking()
     {
-        
+        ranking.SetActive(true);
     }
 }

fe71271d49d64a0e0d420c7370df722ee04f8ff0
JoelEncinas <joelencinasmartin@gmail.com>
2022-04-28 20:30:47 +0200

    inventory ready

--- Assets/Scripts/Menus/Inventory/InventoryUI.cs
+++ Assets/Scripts/Menus/Inventory/InventoryUI.cs
@@ -3,11 +3,14 @@ using System.Collections.Specialized;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class InventoryUI : MonoBehaviour
 {
     // UI
     GameObject skillsContainer;
+    [SerializeField] private List<Sprite> frameColors = new List<Sprite>();
+
 
     // Skill Description 
     GameObject skillIcon;
@@ -73,7 +76,7 @@ public class InventoryUI : MonoBehaviour
     }
 
 
-    public void ChangeSkillInfo(string skillname)
+    public void GetSkillInfo(string skillname)
     {
         OrderedDictionary skillDictionary = SkillCollection.GetSkillByName(skillname);
 
@@ -81,8 +84,9 @@ public class InventoryUI : MonoBehaviour
         {
             if (child.gameObject.name == skillname)
             {
-                // skillIcon = GameObject.Find("SkillIcon");
-                // skillRarityFrame = GameObject.Find("SkillFrame");
+
+                ChangeIcon(skillDictionary["icon"].ToString());
+                ChangeFrameColor(skillDictionary["skillRarity"].ToString());
                 skillName.GetComponent<TextMeshProUGUI>().text = skillDictionary["name"].ToString();
                 skillRarity.GetComponent<TextMeshProUGUI>().text = skillDictionary["skillRarity"].ToString();
                 skillDescription.GetComponent<TextMeshProUGUI>().text = skillDictionary["description"].ToString();
@@ -91,10 +95,56 @@ public class InventoryUI : MonoBehaviour
         }
     }
 
+    public bool HasSkill(string skillname)
+    {
+        int i = 0;
+
+        foreach (Transform skill in skillsList)
+        {
+            if (skillname == (string)SkillCollection.skills[i]["name"])
+            {
+                return true;
+            }
+
+            i++;
+        }
+
+        return false;
+    }
+
     public void GetSkillClicked()
     {
         lastButtonClicked =  EventSystem.current.currentSelectedGameObject.name;
-        ChangeSkillInfo(lastButtonClicked);
+        if(HasSkill(lastButtonClicked))
+            GetSkillInfo(lastButtonClicked);
     }
 
+    public void ChangeFrameColor(string rarity)
+    {
+        switch (rarity)
+        {
+            case "COMMON":
+                skillRarityFrame.GetComponent<Image>().sprite = frameColors[0];
+                break;
+            case "RARE":
+                skillRarityFrame.GetComponent<Image>().sprite = frameColors[1];
+                break;
+            case "EPIC":
+                skillRarityFrame.GetComponent<Image>().sprite = frameColors[2];
+                break;
+            case "LEGENDARY":
+                skillRarityFrame.GetComponent<Image>().sprite = frameColors[3];
+                break;
+        }
+    }
+
+    public void ChangeIcon(string iconNumber)
+    {
+        switch (iconNumber)
+        {
+            case "1":
+                skillIcon.GetComponent<Image>().sprite = frameColors[0];
+                break;
+        }
+    }
 }

9a1297a799d92df961d6091c83a8b93d59e35358
AdrianDanlos <adrianf1team@gmail.com>
2022-04-29 22:47:22 +0700

    level up ui finished

--- Assets/Scripts/Menus/LevelUp/LevelUp.cs
+++ Assets/Scripts/Menus/LevelUp/LevelUp.cs
@@ -1,16 +1,40 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelUp : MonoBehaviour
 {
     public GameObject RewardStats;
     public GameObject RewardItems;
-    public GameObject buttonContinue;
-    public GameObject buttonOpen;
+    public GameObject buttonShowChest;
+    public GameObject buttonOpenChest;
+    public GameObject buttonGoToMainMenu;
+    public GameObject chestRewardPopUp;
     void Start()
     {
+        AddListenerToButtons();
         RewardStats.SetActive(true);
         RewardItems.SetActive(false);
-        buttonContinue.SetActive(true);
-        buttonOpen.SetActive(false);
+        buttonShowChest.SetActive(true);
+        buttonOpenChest.SetActive(false);
+        chestRewardPopUp.SetActive(false);
+    }
+
+    private void AddListenerToButtons() {
+        buttonShowChest.GetComponent<Button>().onClick.AddListener(() => OnClickShowChest());
+        buttonOpenChest.GetComponent<Button>().onClick.AddListener(() => OnClickOpenChest());
+        buttonGoToMainMenu.GetComponent<Button>().onClick.AddListener(() => OnClickGoToMainMenu());
+    }
+
+    private void OnClickShowChest(){
+        RewardStats.SetActive(false);
+        RewardItems.SetActive(true);
+        buttonOpenChest.SetActive(true);
+        gameObject.SetActive(false);
+    }
+    private void OnClickOpenChest(){
+        chestRewardPopUp.SetActive(true);
+    }
+    private void OnClickGoToMainMenu(){
+        chestRewardPopUp.SetActive(false);
     }
 }
--- Assets/Scripts/Menus/LevelUp/OnClickOpen.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class OnClickOpen : MonoBehaviour
-{
-    public void OnClickOpenHandler()
-    {
-        Debug.Log("Chest opened. Show popup with rewards");
-    }
-}

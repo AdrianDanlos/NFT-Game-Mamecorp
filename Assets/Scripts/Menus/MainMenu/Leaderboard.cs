701cd87b3758c1fe752ec9bec6fff7c9c2de0501
JoelEncinas <joelencinasmartin@gmail.com>
2022-05-03 17:58:15 +0200

    added flags + created basic scripts sctructure

--- Assets/Scripts/Menus/MainMenu/Leaderboard.cs
+++ Assets/Scripts/Menus/MainMenu/Leaderboard.cs
@@ -5,18 +5,31 @@ using UnityEngine;
 public class Leaderboard : MonoBehaviour
 {
     // UI
-    public List<GameObject> playersList;
+    public List<Transform> playersList;
     public GameObject playerProfile;
+    public GameObject playersContainer;
 
-    // Start is called before the first frame update
-    void Start()
+    // Player GameObject Structure
+    // - List_Me
+    //      - icon/level text
+    //      - character_Bg
+    //          - mask
+    //              - character image
+    //      - icon country
+    //      - nickname
+    //      - trophies text
+
+    private void Awake()
     {
-        
+        playerProfile = GameObject.Find("List_Me_Player");
+        playersContainer = GameObject.Find("Content");
+
+        GetAllPlayers();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void GetAllPlayers()
     {
-        
+        for(int i = 0; i < playersContainer.transform.childCount; i++)
+            playersList.Add(playersContainer.transform.GetChild(i));
     }
 }

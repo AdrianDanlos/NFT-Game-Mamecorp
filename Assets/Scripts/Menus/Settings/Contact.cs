2d755378ee23098c83565adab31b004ac23ebd78
JoelEncinas <joelencinasmartin@gmail.com>
2022-05-11 03:59:20 +0200

    fixed contact body

---
+++ Assets/Scripts/Menus/Settings/Contact.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Contact : MonoBehaviour
+{
+    GameObject contactBtn;
+
+    [System.Obsolete]
+    private void Awake()
+    {
+        contactBtn = GameObject.Find("Button_Support");
+
+        contactBtn.GetComponent<Button>().onClick.AddListener(() => OnClickOpenMail());
+    }
+
+    // It has to be sanitized or else it will crash silently
+
+    [System.Obsolete]
+    private void OnClickOpenMail()
+    {
+        string email = "mamecorp@gmail.com";
+        string subject = MyEscapeURL("Fight Game");
+        string body = MyEscapeURL("");
+        Application.OpenURL("mailto:" + email + "?subject=" + subject + "&body=" + body);
+    }
+
+    [System.Obsolete]
+    string MyEscapeURL(string url)
+    {
+        return WWW.EscapeURL(url).Replace("+", "%20");
+    }
+}

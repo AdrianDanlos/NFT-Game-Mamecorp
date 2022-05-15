b0db602085c17cfc35d0c2f60c69aab10877d695
JoelEncinas <joelencinasmartin@gmail.com>
2022-05-15 08:40:55 +0200

    added chest opening to dailygift

--- Assets/Scripts/Menus/MainMenu/DailyGift.cs
+++ Assets/Scripts/Menus/MainMenu/DailyGift.cs
@@ -4,6 +4,8 @@ using System;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using TMPro;
+using System.Linq;
+using System.Collections.Specialized;
 
 public class DailyGift : MonoBehaviour
 {
@@ -21,6 +23,23 @@ public class DailyGift : MonoBehaviour
     TextMeshProUGUI goldOrGemsTitle;
     TextMeshProUGUI goldQuantity;
     TextMeshProUGUI gemsQuantity;
+
+    // UI chest rewards
+    public GameObject skillTitle;
+    public GameObject skillType;
+    public GameObject skillRarity;
+    public GameObject skillDescription;
+    public GameObject skillIcon;
+    public GameObject commonSkill;
+    public GameObject rareSkill;
+    public GameObject epicSkill;
+    public GameObject legendarySkill;
+    public Button skillInventory;
+    public Button skillMainMenu;
+    public Button allSkills;
+
+    // data
+    static Fighter player;
 
     // Manager
     MainMenu mainMenu;
@@ -43,6 +62,7 @@ public class DailyGift : MonoBehaviour
         dailyGiftSkills.SetActive(false);
         dailyGiftGoldPopup.SetActive(false);
         dailyGiftGemsPopup.SetActive(false);
+        player = PlayerUtils.FindInactiveFighter();
     }
 
     private void SetUpUI()
@@ -186,7 +206,8 @@ public class DailyGift : MonoBehaviour
 
     private void EnableChestPopup(Dictionary<string, string> reward)
     {
-        // TODO
+        dailyGiftSkills.SetActive(true);
+        SkillPopUpLogic(reward);
     }
 
     public void GiveRewardButton()
@@ -268,5 +289,107 @@ public class DailyGift : MonoBehaviour
         if (PlayerPrefs.GetString("giftCountdown") != "")
             return DateTime.FromBinary(Convert.ToInt64(PlayerPrefs.GetString("giftCountdown"))) - DateTime.Now;
         return TimeSpan.Zero;
+    }
+
+    // handle chest
+    // TODO same code in shop and similar(or same) in level up
+    // create a static script with all 3?
+    private void SkillPopUpLogic(Dictionary<string, string> reward)
+    {
+        SkillCollection.SkillRarity skillRarityAwarded = GetRandomSkillRarityBasedOnChest(reward);
+        Skill skillInstance = GetAwardedSkill(skillRarityAwarded);
+        PlayerUtils.FindInactiveFighter().skills.Add(skillInstance);
+        PlayerUtils.FindInactiveFighter().skills = PlayerUtils.FindInactiveFighter().skills;
+        Notifications.TurnOnNotification();
+        Notifications.IncreaseCardsUnseen();
+
+        ShowSkillData(skillInstance);
+        ShowSkillIcon(skillInstance);
+    }
+
+    private void ShowSkillData(Skill skill)
+    {
+        skillTitle.GetComponent<TextMeshProUGUI>().text = skill.skillName;
+        skillType.GetComponent<TextMeshProUGUI>().text = skill.category;
+        skillRarity.GetComponent<TextMeshProUGUI>().text = skill.rarity;
+        skillDescription.GetComponent<TextMeshProUGUI>().text = skill.description;
+    }
+
+    private bool HasSkillAlready(OrderedDictionary skill)
+    {
+        return player.skills.Any(playerSkill => playerSkill.skillName == skill["name"].ToString());
+    }
+    private Skill GetAwardedSkill(SkillCollection.SkillRarity skillRarityAwarded)
+    {
+        //List of OrderedDictionaries
+        //Filter the ones that are from another rarity and the ones the player already has
+        var skills = SkillCollection.skills
+            .Where(skill => (string)skill["skillRarity"] == skillRarityAwarded.ToString())
+            .Where(skill => !HasSkillAlready(skill))
+            .ToList();
+
+        Debug.Log(SkillCollection.skills
+            .Where(skill => !HasSkillAlready(skill)).ToList().Count + " " + skillRarityAwarded);
+
+        //If player has all skill for the current rarity get skills from a rarity above. 
+        //Does not matter that they might not belong to the current chest
+        if (!skills.Any())
+        {
+            Debug.Log("User has all skills for the given rarity.");
+
+            //Cast enum to int
+            int skillRarityIndex = (int)skillRarityAwarded;
+
+            //If value for the next index in the enum exists return that rarity. Otherwise return the first value of the enum (COMMON)
+            SkillCollection.SkillRarity newRarity = (Enum.IsDefined(typeof(SkillCollection.SkillRarity), (SkillCollection.SkillRarity)skillRarityIndex++) && skillRarityIndex < 4)
+            ? (SkillCollection.SkillRarity)skillRarityIndex++
+            : (SkillCollection.SkillRarity)0;
+
+            //Recursive call with the new rarity
+            GetAwardedSkill(newRarity);
+        }
+
+        int skillIndex = UnityEngine.Random.Range(0, skills.Count());
+
+        Debug.Log(skills.Count);
+        //OrderedDictionary
+        var awardedSkill = skills[skillIndex];
+
+        return new Skill(awardedSkill["name"].ToString(), awardedSkill["description"].ToString(),
+                awardedSkill["skillRarity"].ToString(), awardedSkill["category"].ToString(), awardedSkill["icon"].ToString());
+    }
+
+    private SkillCollection.SkillRarity GetRandomSkillRarityBasedOnChest(Dictionary<string, string> reward)
+    {
+        Dictionary<SkillCollection.SkillRarity, float> skillRarityProbabilitiesForChest =
+            Chest.shopChests[(Chest.ShopChestTypes)Enum.Parse
+            (typeof(Chest.ShopChestTypes), reward["chest"].ToUpper())];
+
+        float diceRoll = UnityEngine.Random.Range(0f, 100);
+
+        foreach (KeyValuePair<SkillCollection.SkillRarity, float> skill in skillRarityProbabilitiesForChest)
+        {
+            if (skill.Value >= diceRoll)
+                return skill.Key;
+
+            diceRoll -= skill.Value;
+        }
+
+        Debug.LogError("Error");
+        //Fallback
+        return SkillCollection.SkillRarity.COMMON;
+    }
+
+    private void ShowSkillIcon(Skill skill)
+    {
+        //ShowFrame
+        commonSkill.SetActive(SkillCollection.SkillRarity.COMMON == GeneralUtils.StringToSkillRarityEnum(skill.rarity));
+        rareSkill.SetActive(SkillCollection.SkillRarity.RARE == GeneralUtils.StringToSkillRarityEnum(skill.rarity));
+        epicSkill.SetActive(SkillCollection.SkillRarity.EPIC == GeneralUtils.StringToSkillRarityEnum(skill.rarity));
+        legendarySkill.SetActive(SkillCollection.SkillRarity.LEGENDARY == GeneralUtils.StringToSkillRarityEnum(skill.rarity));
+
+        //Show icon
+        Sprite icon = Resources.Load<Sprite>("Icons/IconsSkills/" + skill.icon);
+        skillIcon.GetComponent<Image>().sprite = icon;
     }
 }

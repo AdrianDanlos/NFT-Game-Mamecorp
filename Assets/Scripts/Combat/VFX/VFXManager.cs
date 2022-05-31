7eba35a60a0576488094c278f6c7c624e8f1064d
AdrianDanlos <adrianf1team@gmail.com>
2022-05-31 18:28:32 +0700

    bubble added

--- Assets/Scripts/Combat/VFX/VFXManager.cs
+++ Assets/Scripts/Combat/VFX/VFXManager.cs
@@ -1,10 +1,8 @@
-using System.Collections.Generic;
 using UnityEngine;
 
-//TODO: Try to reuse this class for all of the combat VFX. Currently only for blood.
 public class VFXManager
 {
-    public static string GetRandomClipName()
+    public static string GetRandomBloodClipName()
     {
         var bloodAnimatorController = GameObject.Find("Hit_VFX").GetComponent<Animator>().runtimeAnimatorController;
         int randomIndex = Random.Range(0, bloodAnimatorController.animationClips.Length);

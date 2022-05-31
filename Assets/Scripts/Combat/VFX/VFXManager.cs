ff24852643e0645d8bb75f2b9975a74f86b2db30
AdrianDanlos <adrianf1team@gmail.com>
2022-05-31 13:50:05 +0700

    random blood finished

--- Assets/Scripts/Combat/VFX/VFXManager.cs
+++ Assets/Scripts/Combat/VFX/VFXManager.cs
@@ -1,45 +1,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//TODO: Try to reuse this class for all of the combat VFX
+//TODO: Try to reuse this class for all of the combat VFX. Currently only for blood.
 public class VFXManager
 {
-    private static AnimationClip[] LoadBloodVFX()
+    public static string GetRandomClipName()
     {
-        return Resources.LoadAll<AnimationClip>("Animations/VFX/Blood");
-    }
-
-    public static void SetAnimationClipToAnimator(Fighter fighter)
-    {
-        AnimationClip[] bloodClips = LoadBloodVFX();
-        int randomIndex = Random.Range(0, bloodClips.Length);
-        var bloodAnimatorController = fighter.transform.Find("Hit_VFX").GetComponent<Animator>().runtimeAnimatorController;
-        
-        AnimatorOverrideController aoc = new AnimatorOverrideController(bloodAnimatorController);
-
-        AnimationClip currentClip = bloodAnimatorController.animationClips[0];
-        var anims = new List<KeyValuePair<AnimationClip, AnimationClip>>();
-        anims.Add(new KeyValuePair<AnimationClip, AnimationClip>(currentClip, bloodClips[randomIndex]));
-        aoc.ApplyOverrides(anims);
-        bloodAnimatorController = aoc;
-
-        Debug.Log(randomIndex);
-        Debug.Log(anims[0]);
-        Debug.Log(aoc.animationClips[0]);        
-        Debug.Log(aoc.animationClips.Length);        
-    }
-
-    private static AnimationClip FindAnimation(Animator animator, string name)
-    {
-        foreach (AnimationClip clip in animator.runtimeAnimatorController.animationClips)
-        {
-            Debug.Log(clip.name);
-            if (clip.name == name)
-            {
-                Debug.Log(clip.name);
-                return clip;
-            }
-        }
-        return null;
+        var bloodAnimatorController = GameObject.Find("Hit_VFX").GetComponent<Animator>().runtimeAnimatorController;
+        int randomIndex = Random.Range(0, bloodAnimatorController.animationClips.Length);
+        return bloodAnimatorController.animationClips[randomIndex].name;
     }
 }
